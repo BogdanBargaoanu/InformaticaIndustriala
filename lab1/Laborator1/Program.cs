ba734edfb7fed21882b369d3e970b94784c298a9
BogdanBargaoanu <bn19bba@yahoo.com>
2024-02-27 12:26:04 +0200

    Lab1

---
+++ lab1/Laborator1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Laborator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dianaBumb = "lasa ca ti-l...";
+            Console.WriteLine($"asa-i ca, {dianaBumb}");
+        }
+    }
+    
+}
