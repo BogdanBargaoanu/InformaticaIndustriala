d26475628c6827c2e2fe627526411cd9702c0fad
BogdanBargaoanu <bn19bba@yahoo.com>
2024-03-12 19:20:03 +0200

    Polish tema1

--- Tema1/Form1.Designer.cs
+++ Tema1/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -57,6 +58,7 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 564);
             this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // listBox2
             // 
@@ -66,6 +68,7 @@
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(120, 564);
             this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // listBox3
             // 
@@ -75,6 +78,7 @@
             this.listBox3.Name = "listBox3";
             this.listBox3.Size = new System.Drawing.Size(120, 564);
             this.listBox3.TabIndex = 2;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
             // 
             // textBox1
             // 
@@ -165,6 +169,7 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.panel1);
             this.groupBox2.Controls.Add(this.groupBox1);
             this.groupBox2.Controls.Add(this.label3);
@@ -213,6 +218,17 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button3.Location = new System.Drawing.Point(3, 473);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(588, 47);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Adauga";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -252,6 +268,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Tema1/Form1.cs
+++ Tema1/Form1.cs
@@ -12,6 +12,8 @@ namespace Tema1
 {
     public partial class Form1 : Form
     {
+        private long id = -1;
+        private bool adaugare = false;
         public Form1()
         {
             InitializeComponent();
@@ -19,10 +21,20 @@ namespace Tema1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            listBox1.Enabled = false;
-            listBox2.Enabled = false;
-            listBox3.Enabled = false;
+            panel1.Hide();
+            if (!System.IO.File.Exists("reminders.txt"))
+            {
+                System.IO.File.Create("reminders.txt").Close();
+            }
             string[] lines = System.IO.File.ReadAllLines("reminders.txt");
+            if(lines.Length == 0)
+            {
+                textBox1.Enabled = false;
+                richTextBox1.Enabled = false;
+                dateTimePicker1.Enabled = false;
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+            }
             for (int i = 0; i < lines.Length; i++)
             {
                 string[] info = lines[i].Split(' ');
@@ -48,6 +60,12 @@ namespace Tema1
             richTextBox1.Text = "";
             dateTimePicker1.Value = DateTime.Now;
             radioButton1.Checked = radioButton2.Checked = false;
+            panel1.Hide();
+            button3.Show();
+            listBox1.Enabled = true;
+            listBox2.Enabled = true;
+            listBox3.Enabled = true;
+            adaugare = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -76,17 +94,36 @@ namespace Tema1
                 groupBox1.Focus();
                 return;
             }
-            listBox1.Items.Add(textBox1.Text);
-            listBox2.Items.Add(dateTimePicker1.Value.ToString("dd/MM/yyyy"));
-            listBox3.Items.Add(richTextBox1.Text);
-            var writer = System.IO.File.AppendText("reminders.txt");
-            writer.WriteLine(textBox1.Text + " " + dateTimePicker1.Value.ToString("dd/MM/yyyy") + " " + richTextBox1.Text);
-            writer.Close();
-            textBox1.Text = "";
-            richTextBox1.Text = "";
-            dateTimePicker1.Value = DateTime.Now;
-            radioButton1.Checked = radioButton2.Checked = false;
+
+            if(adaugare == true)
+            {
+                listBox1.Items.Add(textBox1.Text);
+                listBox2.Items.Add(dateTimePicker1.Value.ToString("dd/MM/yyyy"));
+                listBox3.Items.Add(richTextBox1.Text);
+                var writer = System.IO.File.AppendText("reminders.txt");
+                writer.WriteLine($"{textBox1.Text} {dateTimePicker1.Value.ToString("dd/MM/yyyy")} {richTextBox1.Text} {radioButton1.Checked} {radioButton2.Checked} {System.DateTime.Now.ToString("yyyyMMddHHmmss")}");
+                writer.Close();
+                textBox1.Text = "";
+                richTextBox1.Text = "";
+                dateTimePicker1.Value = DateTime.Now;
+                radioButton1.Checked = radioButton2.Checked = false;
+            }
+            else
+            {
+                lines = System.IO.File.ReadAllLines("reminders.txt");
+                lines[listBox1.SelectedIndex] = $"{textBox1.Text} {dateTimePicker1.Value.ToString("dd/MM/yyyy")} {richTextBox1.Text} {radioButton1.Checked} {radioButton2.Checked} {System.DateTime.Now.ToString("yyyyMMddHHmmss")}";
+                System.IO.File.WriteAllLines("reminders.txt", lines);
+                listBox1.Items[listBox1.SelectedIndex] = textBox1.Text;
+                listBox2.Items[listBox2.SelectedIndex] = dateTimePicker1.Value.ToString("dd/MM/yyyy");
+                listBox3.Items[listBox3.SelectedIndex] = richTextBox1.Text;
+            }
             SetStare(false);
+            panel1.Hide();
+            button3.Show();
+            listBox1.Enabled = true;
+            listBox2.Enabled = true;
+            listBox3.Enabled = true;
+            adaugare = false;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -94,6 +131,8 @@ namespace Tema1
             if(textBox1.ContainsFocus)
             {
                 SetStare(true);
+                button3.Hide();
+                panel1.Show();
             }
         }
 
@@ -102,6 +141,8 @@ namespace Tema1
             if(dateTimePicker1.ContainsFocus)
             {
                 SetStare(true);
+                button3.Hide();
+                panel1.Show();
             }
         }
 
@@ -110,6 +151,8 @@ namespace Tema1
             if(richTextBox1.ContainsFocus)
             {
                 SetStare(true);
+                button3.Hide();
+                panel1.Show();
             }
         }
 
@@ -118,6 +161,8 @@ namespace Tema1
             if(groupBox1.ContainsFocus)
             {
                 SetStare(true);
+                button3.Hide();
+                panel1.Show();
             }
         }
 
@@ -126,6 +171,77 @@ namespace Tema1
             if (groupBox1.ContainsFocus)
             {
                 SetStare(true);
+                button3.Hide();
+                panel1.Show();
+            }
+        }
+        string[] lines;
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBox2.ContainsFocus && listBox2.SelectedIndex >= 0)
+            {
+                listBox1.SelectedIndex = listBox2.SelectedIndex;
+                listBox3.SelectedIndex = listBox2.SelectedIndex;
+                lines = System.IO.File.ReadAllLines("reminders.txt");
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                richTextBox1.Text = listBox3.SelectedItem.ToString();
+                dateTimePicker1.Value = DateTime.Parse(listBox2.SelectedItem.ToString());
+                radioButton1.Checked = lines[listBox2.SelectedIndex].Split(' ')[3] == "True" ? true : false;
+                radioButton2.Checked = lines[listBox2.SelectedIndex].Split(' ')[4] == "True" ? true : false;
+            }
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBox1.ContainsFocus && listBox1.SelectedIndex >= 0)
+            {
+                listBox2.SelectedIndex = listBox1.SelectedIndex;
+                listBox3.SelectedIndex = listBox1.SelectedIndex;
+                lines = System.IO.File.ReadAllLines("reminders.txt");
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                richTextBox1.Text = listBox3.SelectedItem.ToString();
+                dateTimePicker1.Value = DateTime.Parse(listBox2.SelectedItem.ToString());
+                radioButton1.Checked = lines[listBox2.SelectedIndex].Split(' ')[3] == "True" ? true : false;
+                radioButton2.Checked = lines[listBox2.SelectedIndex].Split(' ')[4] == "True" ? true : false;
+            }
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBox3.ContainsFocus && listBox3.SelectedIndex >= 0)
+            {
+                listBox1.SelectedIndex = listBox3.SelectedIndex;
+                listBox2.SelectedIndex = listBox3.SelectedIndex;
+                lines = System.IO.File.ReadAllLines("reminders.txt");
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                richTextBox1.Text = listBox3.SelectedItem.ToString();
+                dateTimePicker1.Value = DateTime.Parse(listBox2.SelectedItem.ToString());
+                radioButton1.Checked = lines[listBox2.SelectedIndex].Split(' ')[3] == "True" ? true : false;
+                radioButton2.Checked = lines[listBox2.SelectedIndex].Split(' ')[4] == "True" ? true : false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Hide();
+            panel1.Show();
+            adaugare = true;
+            listBox1.SelectedIndex = listBox2.SelectedIndex = listBox3.SelectedIndex = -1;
+            listBox1.Enabled = false;
+            listBox2.Enabled = false;
+            listBox3.Enabled = false;
+            textBox1.Text = "";
+            richTextBox1.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+            radioButton1.Checked = radioButton2.Checked = false;
+            if(textBox1.Enabled == false)
+            {
+                textBox1.Enabled = true;
+                richTextBox1.Enabled = true;
+                dateTimePicker1.Enabled = true;
+                radioButton1.Enabled = true;
+                radioButton2.Enabled = true;
             }
         }
     }
