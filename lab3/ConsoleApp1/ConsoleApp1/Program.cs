93e6953ff6e5008410de1bceb66f3958b6426756
BogdanBargaoanu <bn19bba@yahoo.com>
2024-04-09 13:00:16 +0300

    final lab3 linq

---
+++ lab3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndex = 0;
+            int maxRows = 20;
+
+            using(dbObjectEntities entities = new dbObjectEntities())
+            {
+                IList<Facultati> facultatiList = (from Facultati in entities.Facultatis select Facultati)
+                    .OrderBy(Facultati => Facultati.Id)
+                    .Skip(startIndex).Take(maxRows) .ToList();
+                foreach(Facultati cust in facultatiList)
+                {
+                    Console.WriteLine($"Nume facultate: {cust.NumeFac} , cod: {cust.Code} , id: {cust.Id}");
+                }
+            }
+        }
+    }
+}
---
+++ lab3/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(DataClasses1DataContext db = new DataClasses1DataContext())
+            {
+                try
+                {
+                    var query = from Facultati in db.Facultatis
+                                select new
+                                {
+                                    Id = Facultati.Id,
+                                    NumeFac = Facultati.NumeFac,
+                                    Code  = Facultati.Code
+                                };
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine($"Nume fac: {item.NumeFac}, cod: {item.Code}, id: {item.Id}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
