68c7eb3d79a5bc9461aea0f8c14434ef30bf138f
BogdanBargaoanu <bn19bba@yahoo.com>
2024-04-21 00:58:49 +0300

    button events

--- Tema2/WebApp/Client/TaskManagement.Designer.cs
+++ Tema2/WebApp/Client/TaskManagement.Designer.cs
@@ -30,14 +30,36 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.taskBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.taskNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dueDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.idUserDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.taskBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonRenunta = new System.Windows.Forms.Button();
+            this.buttonSalveaza = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSterge = new System.Windows.Forms.Button();
+            this.buttonAdauga = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.taskBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -52,54 +74,217 @@
             this.dueDateDataGridViewTextBoxColumn,
             this.idUserDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.taskBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 254);
+            this.dataGridView1.Size = new System.Drawing.Size(959, 265);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // taskBindingSource
-            // 
-            this.taskBindingSource.DataSource = typeof(Client.localhost.Task);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // idDataGridViewTextBoxColumn
             // 
             this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
             this.idDataGridViewTextBoxColumn.HeaderText = "id";
             this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // taskNameDataGridViewTextBoxColumn
             // 
             this.taskNameDataGridViewTextBoxColumn.DataPropertyName = "taskName";
             this.taskNameDataGridViewTextBoxColumn.HeaderText = "taskName";
             this.taskNameDataGridViewTextBoxColumn.Name = "taskNameDataGridViewTextBoxColumn";
+            this.taskNameDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // descriptionDataGridViewTextBoxColumn
             // 
             this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "description";
             this.descriptionDataGridViewTextBoxColumn.HeaderText = "description";
             this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // dueDateDataGridViewTextBoxColumn
             // 
             this.dueDateDataGridViewTextBoxColumn.DataPropertyName = "dueDate";
             this.dueDateDataGridViewTextBoxColumn.HeaderText = "dueDate";
             this.dueDateDataGridViewTextBoxColumn.Name = "dueDateDataGridViewTextBoxColumn";
+            this.dueDateDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // idUserDataGridViewTextBoxColumn
             // 
             this.idUserDataGridViewTextBoxColumn.DataPropertyName = "idUser";
             this.idUserDataGridViewTextBoxColumn.HeaderText = "idUser";
             this.idUserDataGridViewTextBoxColumn.Name = "idUserDataGridViewTextBoxColumn";
+            this.idUserDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // taskBindingSource
+            // 
+            this.taskBindingSource.DataSource = typeof(Client.localhost.Task);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(91, 53);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Task name:";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.dateTimePicker1);
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Panel2.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(965, 548);
+            this.splitContainer1.SplitterDistance = 284;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(965, 284);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tasks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Due date:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(91, 98);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(218, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.buttonRenunta);
+            this.panel2.Controls.Add(this.buttonSalveaza);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 226);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(965, 34);
+            this.panel2.TabIndex = 6;
+            // 
+            // buttonRenunta
+            // 
+            this.buttonRenunta.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonRenunta.Location = new System.Drawing.Point(92, 0);
+            this.buttonRenunta.Name = "buttonRenunta";
+            this.buttonRenunta.Size = new System.Drawing.Size(92, 34);
+            this.buttonRenunta.TabIndex = 2;
+            this.buttonRenunta.Text = "Renunta";
+            this.buttonRenunta.UseVisualStyleBackColor = true;
+            this.buttonRenunta.Click += new System.EventHandler(this.buttonRenunta_Click);
+            // 
+            // buttonSalveaza
+            // 
+            this.buttonSalveaza.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonSalveaza.Location = new System.Drawing.Point(0, 0);
+            this.buttonSalveaza.Name = "buttonSalveaza";
+            this.buttonSalveaza.Size = new System.Drawing.Size(92, 34);
+            this.buttonSalveaza.TabIndex = 1;
+            this.buttonSalveaza.Text = "Salveaza";
+            this.buttonSalveaza.UseVisualStyleBackColor = true;
+            this.buttonSalveaza.Click += new System.EventHandler(this.buttonSalveaza_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(339, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(614, 183);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Description";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(608, 164);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonSterge);
+            this.panel1.Controls.Add(this.buttonAdauga);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(965, 31);
+            this.panel1.TabIndex = 3;
+            // 
+            // buttonSterge
+            // 
+            this.buttonSterge.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonSterge.Location = new System.Drawing.Point(92, 0);
+            this.buttonSterge.Name = "buttonSterge";
+            this.buttonSterge.Size = new System.Drawing.Size(92, 31);
+            this.buttonSterge.TabIndex = 1;
+            this.buttonSterge.Text = "Sterge";
+            this.buttonSterge.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdauga
+            // 
+            this.buttonAdauga.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonAdauga.Location = new System.Drawing.Point(0, 0);
+            this.buttonAdauga.Name = "buttonAdauga";
+            this.buttonAdauga.Size = new System.Drawing.Size(92, 31);
+            this.buttonAdauga.TabIndex = 0;
+            this.buttonAdauga.Text = "Adauga";
+            this.buttonAdauga.UseVisualStyleBackColor = true;
+            this.buttonAdauga.Click += new System.EventHandler(this.buttonAdauga_Click);
             // 
             // TaskManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(965, 548);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "TaskManagement";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Task management";
@@ -107,6 +292,15 @@
             this.Load += new System.EventHandler(this.TaskManagement_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.taskBindingSource)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -120,5 +314,19 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dueDateDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn idUserDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonSterge;
+        private System.Windows.Forms.Button buttonAdauga;
+        private System.Windows.Forms.Button buttonRenunta;
+        private System.Windows.Forms.Button buttonSalveaza;
     }
 }
--- Tema2/WebApp/Client/TaskManagement.cs
+++ Tema2/WebApp/Client/TaskManagement.cs
@@ -15,20 +15,37 @@ namespace Client
         private Form frmParinte;
         private localhost.WebService1 webService;
         private int idUser;
+        private bool adaugare = false;
         private Task[] tasks;
+        private Task currentTask = null;
         public TaskManagement(Form frmParinte, localhost.WebService1 webService, int idUser)
         {
             InitializeComponent();
             this.frmParinte = frmParinte; 
             this.webService = webService;
             this.idUser = idUser;
+            // predifined tasks
+            comboBox1.Items.Add("Complete Project Proposal");
+            comboBox1.Items.Add("Create Project Plan");
+            comboBox1.Items.Add("Create Project Schedule");
+            comboBox1.Items.Add("Create Project Budget");
+            comboBox1.Items.Add("Prepare Presentation Slides");
+            comboBox1.Items.Add("Conduct Research");
+            comboBox1.Items.Add("Schedule Meetings");
+            comboBox1.Items.Add("Send Status Update Email");
+            comboBox1.Items.Add("Review Documents");
+            comboBox1.Items.Add("Test Application");
+            comboBox1.Items.Add("Update Project Plan");
+            comboBox1.Items.Add("Review Code Changes");
+            comboBox1.Items.Add("Debug Software Issues");
         }
         private void TaskManagement_Load(object sender, EventArgs e)
         {
             try
             {
-                this.tasks = webService.GetTasks();
+                this.tasks = webService.GetTasksByUser(idUser);
                 dataGridView1.DataSource = tasks;
+                SetStareForm(false);
             }
             catch(Exception ex)
             {
@@ -36,11 +53,150 @@ namespace Client
                 this.Close();
             }
         }
+
+        private void SetStareForm(bool stare)
+        {
+            dataGridView1.Focus();
+            dataGridView1.Enabled = !stare;
+            buttonAdauga.Enabled = !stare;
+            buttonSterge.Enabled = !stare;
+            panel2.Enabled = stare;
+        }
+
+        private void VerificareGridView()
+        {
+            if(this.tasks.Length == 0)
+            {
+                buttonSterge.Enabled = false;
+            }
+            else
+            {
+                buttonSterge.Enabled = true;
+            }
+        }
         private void TaskManagement_FormClosed(object sender, FormClosedEventArgs e)
         {
             frmParinte.Close();
         }
 
-        
+        private void buttonAdauga_Click(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem = null;
+            dateTimePicker1.Value = DateTime.Now;
+            adaugare = true;
+            SetStareForm(true);
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            Task currentTask = (Task)dataGridView1.CurrentRow.DataBoundItem;
+            if(currentTask != null)
+            {
+                comboBox1.SelectedItem = currentTask.taskName;
+                dateTimePicker1.Value = currentTask.dueDate;
+                this.currentTask = currentTask;
+            }
+        }
+
+        private void buttonRenunta_Click(object sender, EventArgs e)
+        {
+            SetStareForm(false);
+            //refresh data
+            dataGridView1_SelectionChanged(null, null);
+            adaugare = false;
+            VerificareGridView();
+        }
+
+        private void buttonSalveaza_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a task.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                comboBox1.Focus();
+                comboBox1.DroppedDown = true;
+                return;
+            }
+            if(dateTimePicker1.Value < DateTime.Now.Date && adaugare)
+            {
+                MessageBox.Show("Please select a valid due date.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dateTimePicker1.Focus();
+                return;
+            }
+            if(!adaugare)
+            {
+                if(currentTask != null)
+                {
+                    try
+                    {
+                        webService.UpdateTask(currentTask.id, comboBox1.SelectedItem.ToString(), richTextBox1.Text, dateTimePicker1.Value);
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show($"An error occurred. Please try again.{ex.Message}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    webService.AddTask(comboBox1.SelectedItem.ToString(), richTextBox1.Text, dateTimePicker1.Value, idUser);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show($"An error occurred. Please try again.{ex.Message}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            this.tasks = webService.GetTasksByUser(idUser);
+            SetStareForm(false);
+
+            // refresh datasource
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = tasks;
+
+            // ensure DataGridView has finished updating
+            dataGridView1.Refresh();
+            Application.DoEvents();
+
+            if(adaugare)
+            {
+                if (dataGridView1.Rows.Count > 0)
+                {
+                    dataGridView1.ClearSelection();
+                    dataGridView1.Rows[dataGridView1.Rows.Count - 1].Selected = true;
+                }
+            }
+            else
+            {
+                int index = dataGridView1.CurrentCell.RowIndex;
+                if (dataGridView1.Rows.Count > 0)
+                {
+                    dataGridView1.ClearSelection();
+                    dataGridView1.Rows[index].Selected = true;
+                }
+            }
+
+            adaugare = false;
+            VerificareGridView();
+            MessageBox.Show("Task saved successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.ContainsFocus)
+            {
+                SetStareForm(true);
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            if(dateTimePicker1.ContainsFocus)
+            {
+                SetStareForm(true);
+            }
+        }
     }
 }
