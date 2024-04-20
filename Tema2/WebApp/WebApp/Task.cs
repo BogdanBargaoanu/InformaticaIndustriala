f5deb4761b0f957f06d5f5bbfe2da124a774a6f3
BogdanBargaoanu <bn19bba@yahoo.com>
2024-04-20 15:31:26 +0300

    finalizat webservice

---
+++ Tema2/WebApp/WebApp/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp
+{
+    public class Task
+    {
+        public int id { get; set; }
+        public string taskName { get; set; }
+        public string description { get; set; }
+        public DateTime dueDate { get; set; }
+        public int idUser { get; set; }
+    }
+}
--- Tema2/WebApp/WebApp/WebService1.asmx.cs
+++ Tema2/WebApp/WebApp/WebService1.asmx.cs
@@ -18,7 +18,7 @@ namespace WebApp
     // [System.Web.Script.Services.ScriptService]
     public class WebService1 : System.Web.Services.WebService
     {
-
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\InformaticaIndustriala\\Tema2\\WebApp\\Client\\Database1.mdf;Integrated Security=True";
         [WebMethod]
         public string HelloWorld()
         {
@@ -28,7 +28,7 @@ namespace WebApp
         [WebMethod]
         public List<User> GetUsers()
         {
-            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\InformaticaIndustriala\\Tema2\\WebApp\\Client\\Database1.mdf;Integrated Security=True";
+            string connectionString = this.connectionString;
             var users = new List<User>();
 
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -60,7 +60,7 @@ namespace WebApp
         [WebMethod]
         public bool AddUser(string username, string password)
         {
-            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\InformaticaIndustriala\\Tema2\\WebApp\\Client\\Database1.mdf;Integrated Security=True";
+            string connectionString = this.connectionString;
 
             try
             {
@@ -82,5 +82,153 @@ namespace WebApp
                 return false;
             }
         }
+
+        [WebMethod]
+        public List<Task> GetTasks()
+        {
+            string connectionString = this.connectionString;
+            var tasks = new List<Task>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Tasks";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var task = new Task
+                        {
+                            id = reader.GetInt32(reader.GetOrdinal("id")),
+                            taskName = reader.GetString(reader.GetOrdinal("taskName")),
+                            description = reader.GetString(reader.GetOrdinal("description")),
+                            dueDate = reader.GetDateTime(reader.GetOrdinal("dueDate")),
+                            idUser = reader.GetInt32(reader.GetOrdinal("idUser")),
+                        };
+
+                        tasks.Add(task);
+                    }
+                }
+            }
+
+            return tasks;
+        }
+        [WebMethod]
+        public List<Task> GetTasksByUser(int idUser)
+        {
+            string connectionString = this.connectionString;
+            var tasks = new List<Task>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Tasks WHERE idUser = @idUser";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue(@"idUser", idUser);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var task = new Task
+                        {
+                            id = reader.GetInt32(reader.GetOrdinal("id")),
+                            taskName = reader.GetString(reader.GetOrdinal("taskName")),
+                            description = reader.GetString(reader.GetOrdinal("description")),
+                            dueDate = reader.GetDateTime(reader.GetOrdinal("dueDate")),
+                            idUser = reader.GetInt32(reader.GetOrdinal("idUser")),
+                        };
+
+                        tasks.Add(task);
+                    }
+                }
+            }
+
+            return tasks;
+        }
+
+        [WebMethod]
+        public bool AddTask(string taskName, string description, DateTime dueDate, int idUser)
+        {
+            string connectionString = this.connectionString;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO Tasks (taskName, description, dueDate, idUser) VALUES (@taskName, @description, @dueDate, @idUser)";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@taskName", taskName);
+                    command.Parameters.AddWithValue("@description", description);
+                    command.Parameters.AddWithValue("@dueDate", dueDate);
+                    command.Parameters.AddWithValue("@idUser", idUser);
+
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        [WebMethod]
+        public bool DeleteTask(int id)
+        {
+            string connectionString = this.connectionString;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "DELETE FROM Tasks WHERE id = @id";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@id", id);
+
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        [WebMethod]
+        public bool UpdateTask(int id, string taskName, string description, DateTime dueDate)
+        {
+            string connectionString = this.connectionString;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "UPDATE Tasks SET taskName = @taskName, description = @description, dueDate = @dueDate WHERE id = @id";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@taskName", taskName);
+                    command.Parameters.AddWithValue("@description", description);
+                    command.Parameters.AddWithValue("@dueDate", dueDate);
+                    command.Parameters.AddWithValue("@id", id);
+
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }   
     }
 }
