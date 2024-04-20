3f58d91b04d418ab313ca3fe61ac5c119ef0bfd8
BogdanBargaoanu <bn19bba@yahoo.com>
2024-04-20 14:32:45 +0300

    login form

---
+++ Tema2/WebApp/WebApp/WebService1.asmx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApp
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public List<User> GetUsers()
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\InformaticaIndustriala\\Tema2\\WebApp\\Client\\Database1.mdf;Integrated Security=True";
+            var users = new List<User>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Users";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var user = new User
+                        {
+                            id = reader.GetInt32(reader.GetOrdinal("id")),
+                            username = reader.GetString(reader.GetOrdinal("username")),
+                            password = reader.GetString(reader.GetOrdinal("password")),
+                        };
+
+                        users.Add(user);
+                    }
+                }
+            }
+
+            return users;
+        }
+    }
+}
