f4cd19ed93140c27e2bae2d17aa55d5b1a2af332
BogdanBargaoanu <bn19bba@yahoo.com>
2024-04-23 13:54:44 +0300

    final lab6

---
+++ lab6/App1/Data/EmployeeManagementSystemDbContext.cs
@@ -0,0 +1,14 @@
+﻿using App1.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace App1.Data
+{
+    public class EmployeeManagementSystemDbContext : DbContext
+    {
+        public EmployeeManagementSystemDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
